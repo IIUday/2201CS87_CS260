8c4212867ff2ae16f2edcfc05e565a9ddf757fe6
IIUday <118089448+IIUday@users.noreply.github.com>
2024-04-09 10:55:28 +0530

    Update tut05_answers.sql

--- tut05/tut05_answers.sql
+++ tut05/tut05_answers.sql
@@ -7,3 +7,4 @@ select * from employees Join projects;
 select * from departments NATURAL JOIN projects;
 select * from projects where budget>100000;
 (select * from employees where department_id=(select department_id from departments where department_name="Engineering")) UNION (select * from employees where department_id=(select department_id from departments where department_name="Finance"))
+select * from employees JOIN projects;
