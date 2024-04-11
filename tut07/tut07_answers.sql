adf2442eecbb19a5578428a76dfbb2f9511a5528
IIUday <118089448+IIUday@users.noreply.github.com>
2024-04-11 16:36:29 +0530

    Update tut07_answers.sql

--- tut07/tut07_answers.sql
+++ tut07/tut07_answers.sql
@@ -1,3 +1,5 @@
 -- General Instructions
 -- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
 -- Comment in MYSQL 
+select AVERAGE(salary),department from employees GROUP BY department_id;
+UPDATE employees SET  salary= (salary + 0.1*salary);
