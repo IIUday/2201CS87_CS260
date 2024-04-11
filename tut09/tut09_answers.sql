823e4c403d989e3011f816574e08b218488e688b
IIUday <118089448+IIUday@users.noreply.github.com>
2024-04-11 22:22:26 +0530

    Update tut09_answers.sql

--- tut09/tut09_answers.sql
+++ tut09/tut09_answers.sql
@@ -6,8 +6,10 @@ SELECT player-name, FLOOR(DATEDIFF('2018-12-02', dob) / 365) AS age FROM player 
 select match-id,toss-winner from match where toss-decision="bat" ORDER BY match-id;
 SELECT over-id, runs-scored FROM batsman_scored WHERE match_id = 335987 AND runs-scored <= 7 ORDER BY runs_scored DESC, over_id;
 select player-name from player where player-id=(select bowler from ball_by_ball);
-select match-id,team-1,team-2,win-margin from match where win-margin>60 or win-margin=60 ORDER BY win-margin ASC, match-id;
+select match-id,team-1,team-2,win-margin,name from match JOIN team where win-margin>=60 AND match-winner=team-id ORDER BY win-margin ASC, match-id;
 SELECT player-name FROM player where FLOOR(DATEDIFF('2018-12-02', dob) / 365)<30 ORDER BY player-name;
+SELECT match-id,SUM(runs-Scored) as Total from batsman_scored GROUP BY match-id;
+
 
 
 
