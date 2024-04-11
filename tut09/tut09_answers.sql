0fb793adeb0cc6139c18e4a558ecf433c149a899
IIUday <118089448+IIUday@users.noreply.github.com>
2024-04-11 17:29:21 +0530

    Update tut09_answers.sql

--- tut09/tut09_answers.sql
+++ tut09/tut09_answers.sql
@@ -1,3 +1,16 @@
 -- General Instructions
 -- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
 -- Comment in MYSQL 
+select player-name from player where batting-hand="Left-hand-bat" ORDER BY player-name;
+SELECT player-name, 
+       FLOOR(DATEDIFF('2018-12-02', dob) / 365) AS age
+FROM player where bowling-skill="Legbreak googly" AND age>28 ORDER BY age DESC,player-name;
+select match-id,toss-winner from match where toss-decision="bat" ORDER BY match-id;
+SELECT over-id, runs-scored
+FROM batsman_scored
+WHERE match_id = 335987
+  AND runs-scored <= 7
+ORDER BY runs_scored DESC, over_id;
+select player-id 
+
+
