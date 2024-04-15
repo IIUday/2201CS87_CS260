3ee0ae97e7492947bd008dc0475223734a94f111
IIUday <118089448+IIUday@users.noreply.github.com>
2024-04-15 12:44:32 +0530

    Update tut09_answers.sql

--- tut09/tut09_answers.sql
+++ tut09/tut09_answers.sql
@@ -1,16 +1,16 @@
 -- General Instructions
 -- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
 -- Comment in MYSQL 
-select player-name from player where batting-hand="Left-hand-bat" ORDER BY player-name;
-SELECT player-name, FLOOR(DATEDIFF('2018-12-02', dob) / 365) AS age FROM player where bowling-skill="Legbreak googly" AND age>28 ORDER BY age DESC,player-name;
-select match-id,toss-winner from match where toss-decision="bat" ORDER BY match-id;
-SELECT over-id, runs-scored FROM batsman_scored WHERE match_id = 335987 AND runs-scored <= 7 ORDER BY runs_scored DESC, over_id;
-select player-name from player where player-id=(select bowler from ball_by_ball);
-select match-id,team-1,team-2,win-margin,name from match JOIN team where win-margin>=60 AND match-winner=team-id ORDER BY win-margin ASC, match-id;
-SELECT player-name FROM player where FLOOR(DATEDIFF('2018-12-02', dob) / 365)<30 ORDER BY player-name;
-SELECT match-id,SUM(runs-Scored) as Total from batsman_scored GROUP BY match-id;
-select player-name,count(kind-out) wicket_taken JOIN player Where player-out=player-id GROUP BY player-name;
-select kind-out as out_type, count(kind-out) as number from wicket_taken GROUP BY kind-out ORDER BY number DESC,kind-out;
+select player_name from player where batting_hand="Left-hand-bat" ORDER BY player_name;
+SELECT player_name, FLOOR(DATEDIFF('2018-12-02', dob) / 365) AS age FROM player where bowling_skill="Legbreak googly" AND age>28 ORDER BY age DESC,player_name;
+select match_id,toss_winner from match where toss_decision="bat" ORDER BY match_id;
+SELECT over_id, runs_scored FROM batsman_scored WHERE match_id = 335987 AND runs_scored <= 7 ORDER BY runs_scored DESC, over_id;
+select player_name from player where player_id=(select bowler from ball_by_ball);
+select match_id,team-1,team-2,win_margin,name from match JOIN team where win_margin>=60 AND match_winner=team-id ORDER BY win_margin ASC, match_id;
+SELECT player_name FROM player where FLOOR(DATEDIFF('2018-12-02', dob) / 365)<30 ORDER BY player_name;
+SELECT match_id,SUM(runs_Scored) as Total from batsman_scored GROUP BY match-id;
+select player_name,count(kind_out) wicket_taken JOIN player Where player_out=player_id GROUP BY player_name;
+select kind_out as out_type, count(kind_out) as number from wicket_taken GROUP BY kind_out ORDER BY number DESC,kind_out;
 
 
 
